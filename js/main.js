aa29d252267ee5d8d9d1094ee8d3feb6b5c7e804
massimocolella02 <massimo-colella@outlook.it>
2023-03-23 10:25:29 +0100

    Esercizio Completato

--- js/main.js
+++ js/main.js
@@ -2,10 +2,22 @@
 
 function calc() 
 {
-    let km = document.getElementById('kmPasseggero').value;
+    let km =  document.getElementById('kmPasseggero').value;
     let età = document.getElementById('etàPasseggero').value;
-    let prezzokm = 0.21;
-    
-    document.getElementById('risultato').innerHTML= 'Il prezzo del biglietto è: '+ km*prezzokm +'€'
+
+    const prezzokm = 0.21;
+
+    let costo = km * prezzokm
+
+    if ( età < 18 ) {
+        costo = costo * 0.80
+        document.getElementById('risultato').innerHTML= 'Il prezzo del biglietto per minorenni è ' + costo.toFixed(2) + '€'
+    } else if ( età > 65 ) {
+        costo = costo * 0.60
+        document.getElementById('risultato').innerHTML= 'Il prezzo del biglietto per anziani è ' + costo.toFixed(2) + '€'
+    } else {
+        document.getElementById('risultato').innerHTML= 'Il prezzo del biglietto è ' + costo.toFixed(2) + '€'
+    }
 }
-    
+
+
